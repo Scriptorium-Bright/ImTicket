b5033999f15e3d922fe04690bf2df20506c34de1
notYourOcean <wjswjdgus333@gmail.com>
2025-06-16 06:55:51 +0900

    asd

--- src/main/java/org/example/ticket/security/config/WebConfig.java
+++ src/main/java/org/example/ticket/security/config/WebConfig.java
@@ -8,18 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    // application.yml 또는 properties 파일에 설정한 파일 업로드 경로를 주입받습니다.
     @Value("${ticket.upload.path}")
-    private String uploadDir;
+    private String fileUploadDir;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        String resourcePath = "file:" + uploadDir;
-
-        if (!uploadDir.endsWith("/") && !uploadDir.endsWith("\\")) {
-            resourcePath += "/";
-        }
-
-        registry.addResourceHandler("/uploads/**")
-                .addResourceLocations(resourcePath);
+        // "/picture/**" URL 요청이 오면
+        registry.addResourceHandler("/picture/**")
+                // file:///home/ubuntu/capstoneBackend/pathForPhoto/picture/ 에서 파일을 찾아 제공합니다.
+                .addResourceLocations("file:///" + fileUploadDir + "/picture/");
     }
 }
