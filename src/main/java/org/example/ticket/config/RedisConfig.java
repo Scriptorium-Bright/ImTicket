70d4a911aaf8391cc4f6f2ec888b44bc9c64a491
notYourOcean <wjswjdgus333@gmail.com>
2026-01-15 20:53:05 +0900

    refactor : redisConfig 수정

--- src/main/java/org/example/ticket/config/RedisConfig.java
+++ src/main/java/org/example/ticket/config/RedisConfig.java
@@ -5,6 +5,7 @@ import org.example.ticket.venue.stream.SeatCreationProducer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.stream.Consumer;
 import org.springframework.data.redis.connection.stream.MapRecord;
 import org.springframework.data.redis.connection.stream.ReadOffset;
 import org.springframework.data.redis.connection.stream.StreamOffset;
@@ -14,7 +15,10 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.data.redis.stream.StreamMessageListenerContainer;
 import org.springframework.data.redis.stream.Subscription;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.time.Duration;
+import java.util.UUID;
 
 @Configuration
 public class RedisConfig {
@@ -23,15 +27,10 @@ public class RedisConfig {
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
-
-        // Key Serializer: String
         template.setKeySerializer(new StringRedisSerializer());
         template.setHashKeySerializer(new StringRedisSerializer());
-
-        // Value Serializer: JSON
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
-
         return template;
     }
 
@@ -56,35 +55,19 @@ public class RedisConfig {
 
         try {
             if (Boolean.FALSE.equals(redisTemplate.hasKey(streamKey))) {
-                // If stream does not exist, create group (which also creates stream in some
-                // versions, but better safe)
-                // However, XGROUP CREATE requires the stream to exist normally.
-                // We typically handle this by catching the exception or creating a dummy stream
-                // first if needed.
-                // Spring Data Redis 'createGroup' works if we handle it correctly.
-                try {
-                    redisTemplate.opsForStream().createGroup(streamKey, consumerGroup);
-                } catch (Exception e) {
-                    // Ignore if failing because stream doesn't exist, we might need to create it
-                    // explicitly or wait
-                    // Just logging here, in production we might want robust initialization
-                }
+                redisTemplate.opsForStream().createGroup(streamKey, consumerGroup);
             } else {
-                // Determine if group exists
-                try {
-                    redisTemplate.opsForStream().createGroup(streamKey, consumerGroup);
-                } catch (Exception e) {
-                    // Ignore "BUSYGROUP Consumer Group name already exists"
-                }
+                redisTemplate.opsForStream().createGroup(streamKey, consumerGroup);
             }
         } catch (Exception e) {
-            // Ensuring application doesn't crash on startup due to Redis connection issues
-            // if desired
         }
 
+        String consumerName = "consumer-" + UUID.randomUUID().toString().substring(0, 8);
+
         container.start();
+
         return container.receive(
-                org.springframework.data.redis.connection.stream.Consumer.from(consumerGroup, "app-instance-1"),
+                Consumer.from(consumerGroup, consumerName),
                 StreamOffset.create(streamKey, ReadOffset.lastConsumed()),
                 consumer);
     }
